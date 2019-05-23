241030d15ec17c28d1c0cf44a61974f721ce7414
Raguauxo <ragu9921@gmail.com>
2019-05-23 18:24:47 +0530

    java programs

---
+++ src/com/auxo/AllFactors.java
@@ -0,0 +1,26 @@
+package com.auxo;
+
+import java.util.Scanner;
+
+public class AllFactors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = scanner.nextInt();
+        printFactors(number);
+    }
+    public static void printFactors(int number)
+    {
+        if(number>1) {
+            for (int count = 1; count <= 6; count++) {
+                if (number % count == 0) {
+                    System.out.println(count);
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Invalid value");
+        }
+
+}}
--- src/com/auxo/SharedDigit.java
+++ src/com/auxo/SharedDigit.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SharedDigit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter the numbers");
         int number1 = scanner.nextInt();
         int number2 = scanner.nextInt();
         System.out.println(hasSharedDigit(number1, number2));
@@ -13,12 +13,15 @@ public class SharedDigit {
 
     public static boolean hasSharedDigit(int number1, int number2) {
         if (isRange(number1) && isRange(number2)) {
-            if ((number1 % 10) == (number2 % 10) || (number1 / 10) == (number2 % 10) || (number2 % 10) == (number2 / 10) || (number2 / 10) == (number1 % 10)) {
-                return true;
-            } else {
+            if ((number1 % 10) == (number2 % 10) || (number1 / 10) == (number2 % 10))
+                if ((number2 % 10) == (number2 / 10) || (number2 / 10) == (number1 % 10)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            else {
                 return false;
             }
-
         }
         return false;
     }
@@ -26,8 +29,9 @@ public class SharedDigit {
     public static boolean isRange(int number) {
         if (number >= 10 && number <= 99) {
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
 }
